a0b154ef684d13d322034d66030ee48fc0466b95
BMM <m.bazyk@gmail.com>
2026-02-20 17:25:51 +0200

    fix  errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,17 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (error, req, res, next) => {
+  if (error instanceof HttpError) {
+    return res.status(error.status).json({
+      status: error.status,
+      message: error.message,
+    });
+  }
+
   console.error(error);
 
-  res.status(500).json({ message: 'Internal Server Error' });
+  res.status(500).json({
+    status: 500,
+    message: 'Internal Server Error',
+  });
 };
