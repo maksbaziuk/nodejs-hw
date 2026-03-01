10999b45db9158d1f2524028e88e302e4bd243c1
BMM <m.bazyk@gmail.com>
2026-03-01 22:29:27 +0200

    fix content and title for validation

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -4,15 +4,13 @@ import { TAGS } from '../constants/tags.js';
 
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).max(30).required().messages({
+    title: Joi.string().min(1).required().messages({
       'string.base': 'Title must be a string',
       'string.min': 'Title should have at least {#limit} characters',
-      'string.max': 'Title should have at most {#limit} characters',
       'any.required': 'Title is required',
     }),
-    content: Joi.string().max(300).allow('').messages({
+    content: Joi.string().allow('').messages({
       'string.base': 'Content must be a string',
-      'string.max': 'Content should have at most {#limit} characters',
     }),
     tag: Joi.string().valid(...TAGS),
   }),
@@ -34,7 +32,7 @@ export const updateNoteSchema = {
   }),
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).max(30),
-    content: Joi.string().max(300).allow(''),
+    content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   }).min(1),
 };
