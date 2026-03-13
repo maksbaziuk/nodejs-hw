f5f628edee60571f549df2dd5827a4b298da4039
BMM <m.bazyk@gmail.com>
2026-03-13 20:24:57 +0200

    fix multer

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -6,8 +6,7 @@ export const upload = multer({
     fileSize: 2 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
-    const allowedTypes = ['image/'];
-    if (allowedTypes.includes(file.mimetype)) {
+    if (file.mimetype.startsWith('image/')) {
       cb(null, true);
     } else {
       cb(
